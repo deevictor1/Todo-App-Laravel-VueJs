c7e846445a39ed3deff41a5bca36f0ef2cc1c33a
Victor <vicomsms@gmail.com>
2023-01-02 16:36:12 +0100

    Initial commit

---
+++ resources/js/vue/listItem.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="item">
+        <input
+            type="checkbox"
+            @change="updateCheck($event.target.checked)"
+            v-model="item.completed"
+            :checked="isChecked"
+            />
+        <span :class="[item.completed ? 'completed' : '', 'itemText']"> {{ item.name }} </span>
+        <button @click="removeItem" class="trashcan" >
+            <font-awesome-icon icon="trash" />
+        </button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ['item'],
+
+    computed: {
+        isChecked(){
+            return this.item.completed;
+        }
+    },
+
+    methods: {
+        updateCheck() {
+            axios.put('api/item/' + this.item.id, {
+                item: this.item
+            })
+            .then( response => {
+                if( response.status == 200 ) {
+                    
+                }
+            })
+            .catch( error => {
+                console.log( error );
+            })
+        },
+        removeItem() {
+            axios.delete('api/item/' + this.item.id )
+            .then( response => {
+                if ( response.status == 200) {
+                    this.$emit( 'itemchanged');
+                }
+            })
+            .catch ( error => {
+                console.log ( error );
+            })
+        }
+    },
+}
+</script>
+
+<style scoped>
+.completed {
+    text-decoration: line-through;
+    color: #999999;
+}
+.itemText {
+    width: 100%;
+    margin-left: 20px;
+}
+.item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.trashcan {
+    background: #e6e6e6;
+    border: none;
+    color: #FF0000;
+    outline: none;
+}
+</style>
